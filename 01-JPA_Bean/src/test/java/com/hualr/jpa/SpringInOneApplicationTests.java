1c453289819db66f9459bb8147bea752cd18ae9d
zzongqi <3031720178@qq.com>
2020-11-08 22:04:09 +0800

    维护关系比较: 虽然单放弃了维护关系 但是依然可以在保存单的时候一同保存多 但是,无法确认此时多对应着那个单 此时,多表中的外键为空

--- 01-JPA_Bean/src/test/java/com/hualr/jpa/SpringInOneApplicationTests.java
+++ 01-JPA_Bean/src/test/java/com/hualr/jpa/SpringInOneApplicationTests.java
@@ -1,11 +1,17 @@
 package com.hualr.jpa;
 
 import com.hualr.jpa.bean.Klass;
+import com.hualr.jpa.bean.Student;
+import com.hualr.jpa.dao.KlassDao;
+import com.hualr.jpa.dao.StudentDao;
 import com.hualr.jpa.service.api.KlassService;
 import com.hualr.jpa.service.api.StudentService;
+import java.util.Arrays;
+import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 @SpringBootTest
 class SpringInOneApplicationTests {
@@ -13,7 +19,28 @@ class SpringInOneApplicationTests {
     StudentService studentService;
     @Autowired
     KlassService klassService;
+    @Autowired
+    StudentDao studentDao;
+    @Autowired
+    KlassDao klassDao;
+
     @Test
-    void contextLoads() {
+    @Transactional
+    @Rollback(false)
+    public void test1() {
+        Klass klass=new Klass();
+        klass.setClassName("小葵班");
+        Student student1=new Student();
+        student1.setAge(11);
+        student1.setStudentName("网民");
+
+        Student student2=new Student();
+        student2.setAge(11);
+        student2.setStudentName("网民");
+        klass.setStudents(Arrays.asList(student1,student2));
+        /**
+         * 1. 单独save 1 此时,由于1中set了多属性,因此,会顺便save多 但是多对应的1属性是无法拿到的
+         */
+        klassDao.save(klass);
     }
 }
